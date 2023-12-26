5a54d9320c31744ac92c325509dfb20caee9bd20
drewdi <realandrewhunter@gmail.com>
2023-12-25 20:04:09 -0600

    more changes

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ function App() {
             </Text>
             <List as={motion.ul} variants={parentContainer} intial="hidden" animate="show">
               {links.map(link => (
-                <a href={link.url} target="_blank" rel="noopener noreferrer" key={link.text}>
+                <a href={link.url} target="_blank">
                   <HStack
                     w="15em"
                     h="3em"
