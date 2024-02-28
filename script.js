4836e0117b124e43f0fb282a1cb7cdbd0aaad72e
drewdii <realandrewhunter@gmail.com>
2024-02-27 22:30:16 -0500

    more style changes

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
-accordionItemHeaders.forEach(accordionItemHeader => {
-  accordionItemHeader.addEventListener("click", event => {
-     const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-     if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-       currentlyActiveAccordionItemHeader.classList.toggle("active");
-       currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-     }
-    accordionItemHeader.classList.toggle("active");
-    const accordionItemBody = accordionItemHeader.nextElementSibling;
-    if(accordionItemHeader.classList.contains("active")) {
-      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-    }
-    else {
-      accordionItemBody.style.maxHeight = 0;
-    }
+document.addEventListener("DOMContentLoaded", function() {
+  const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+  accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
+      // Close the currently active accordion item if it's not the one being clicked
+      const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+      if (currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader) {
+        currentlyActiveAccordionItemHeader.classList.remove("active");
+        currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+      }
+      // Toggle the active class and maxHeight for the clicked accordion item
+      accordionItemHeader.classList.toggle("active");
+      const accordionItemBody = accordionItemHeader.nextElementSibling;
+      if (accordionItemHeader.classList.contains("active")) {
+        accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+      } else {
+        accordionItemBody.style.maxHeight = 0;
+      }
+    });
   });
 });
